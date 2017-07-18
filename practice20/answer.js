639f3eb8e8364a075b297a295221fd8d9641eafd
jonmorales89 <jon.sal.morales@gmail.com>
2017-07-18 10:39:10 -0700

    my updates

--- practice20/answer.js
+++ practice20/answer.js
@@ -1,1 +1,14 @@
 answer.js
+
+var myArray = [5,3,7,8,2,4,1,2];
+
+function randomize(arr){
+    var newArr = [];
+    for(var i = 0; i < arr.length; i++){
+        var ind = Math.floor((Math.random()*arr.length-1)+1);
+        newArr.push(arr[ind]);
+    }
+    return newArr;
+}
+
+console.log(randomize(myArray));
--- practice21/answer.js
+++ practice21/answer.js
@@ -1,1 +1,9 @@
 answer.js
+
+function bipolarLoop(){
+    for(var i = 0, j = 10; i < 11, j >=0; i++, j--){
+        console.log(i, j);
+    }
+}
+
+bipolarLoop();
